66b8fbd4ce863ca3398e2fa339dd6820abf7903f
Daniel <dmishael91@gmail.com>
2025-04-01 12:17:07 -0400

    seeded db

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -17,39 +17,39 @@ class EmployeeResource extends Resource
 {
     protected static ?string $model = Employee::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+    protected static ?string $navigationGroup = 'Employee Management';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('country_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('state')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('city_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('department_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('first_name')
+                Forms\Components\Section::make('User Name')
+                ->description('Put the user name details in.')
+                ->schema([Forms\Components\TextInput::make('first_name')
                     ->required(),
                 Forms\Components\TextInput::make('last_name')
                     ->required(),
                 Forms\Components\TextInput::make('middle_name')
                     ->required(),
-                Forms\Components\TextInput::make('address')
+                
+            ])->columns(3),
+                Forms\Components\Section::make('User Address')
+                ->schema([
+                    Forms\Components\TextInput::make('address')
                     ->required(),
                 Forms\Components\TextInput::make('zip_code')
                     ->required(),
-                Forms\Components\DatePicker::make('date_of_birth')
+                ])->columns(2),
+                Forms\Components\Section::make('Dates')
+                ->schema([
+                    Forms\Components\DatePicker::make('date_of_birth')
                     ->required(),
                 Forms\Components\DatePicker::make('date_hired')
-                    ->required(),
-            ]);
+                    ->required()
+                ])->columns(2),
+            ])->columns(3);
     }
 
     public static function table(Table $table): Table
